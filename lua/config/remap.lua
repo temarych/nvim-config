c5ce6f68130aa086eb59f50f856ebca5714bdb38
temarych <teemest06@icloud.com>
2025-07-09 13:02:25 +0300

    feat(remap): add remaps and update plugins

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,3 +2,9 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set({ "n", "v" }, '<leader>ca', vim.lsp.buf.code_action)
+
+vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
