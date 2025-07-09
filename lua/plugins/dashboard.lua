203d6cb1ee8ba48429619d7d74dfaccf41b825ff
temarych <teemest06@icloud.com>
2025-07-09 17:00:20 +0300

    feat(dashboard): add startup dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,25 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/tokyonight.nvim' },
+  config = function()
+    local startify = require("alpha.themes.startify")
+
+    startify.file_icons.provider = "devicons"
+
+    startify.section.header.val = {
+      [[ __                                                       __         ]],
+      [[/\ \__                                                   /\ \        ]],
+      [[\ \ ,_\    __    ___ ___      __     _ __   __  __    ___\ \ \___    ]],
+      [[ \ \ \/  /'__`\/' __` __`\  /'__`\  /\`'__\/\ \/\ \  /'___\ \  _ `\  ]],
+      [[  \ \ \_/\  __//\ \/\ \/\ \/\ \L\.\_\ \ \/ \ \ \_\ \/\ \__/\ \ \ \ \ ]],
+      [[   \ \__\ \____\ \_\ \_\ \_\ \__/.\_\\ \_\  \/`____ \ \____\\ \_\ \_\]],
+      [[    \/__/\/____/\/_/\/_/\/_/\/__/\/_/ \/_/   `/___/> \/____/ \/_/\/_/]],
+      [[                                                /\___/               ]],
+      [[                                                \/__/                ]],
+    }
+
+    require("alpha").setup(
+      startify.config
+    )
+  end,
+}
