d0e5024cd67c81363c276bb97e7291726b84d34c
temarych <teemest06@icloud.com>
2025-07-14 12:16:30 +0300

    feat(rust): add rustacenvim plugin for better rust integration

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,10 +1,10 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set({ "n", "v" }, '<leader>ca', vim.lsp.buf.code_action)
 
-vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "go", vim.lsp.buf.type_definition)
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -20,8 +20,26 @@ return {
         exclude = {
           -- "pyright",
           "ty",
+          "rust_analyzer",
         },
       },
     },
+    config = function(_, opts)
+      lspconfig = require("mason-lspconfig")
+
+      lspconfig.setup(opts)
+
+      vim.lsp.config("rust-analyzer", {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>ca", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { silent = true, buffer = bufnr })
+
+          vim.keymap.set("n", "K", function()
+            vim.cmd.RustLsp({ "hover", "actions" })
+          end, { silent = true, buffer = bufnr })
+        end,
+      })
+    end,
   },
 }
