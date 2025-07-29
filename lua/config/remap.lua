3ee2712c6953c6ee58632f8b4a1d0edf4558966a
temarych <teemest06@icloud.com>
2025-07-29 15:13:23 +0300

    feat(remap): update remap & improve cmp

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -8,3 +8,5 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "go", vim.lsp.buf.type_definition)
+
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
