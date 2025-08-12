b4e6b651e1f95ea803961baa99ee1779353b86cb
temarych <teemest06@icloud.com>
2025-08-12 13:14:49 +0300

    feat(todo): add todo comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,25 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>ft", "<cmd>TodoTelescope<cr>",              desc = "Find TODOs (Telescope)" },
+    { "<leader>qt", "<cmd>TodoQuickFix<cr><cmd>copen<cr>", desc = "Quickfix TODOs" },
+    {
+      "]t",
+      function()
+        require("todo-comments").jump_next()
+      end,
+      desc = "Next TODO (buffer)",
+    },
+    {
+      "[t",
+      function()
+        require("todo-comments").jump_prev()
+      end,
+      desc = "Prev TODO (buffer)",
+    },
+    { "]T", "<cmd>cnext<cr>", desc = "Next TODO (quickfix)" },
+    { "[T", "<cmd>cprev<cr>", desc = "Prev TODO (quickfix)" },
+  },
+  opts = {},
+}
